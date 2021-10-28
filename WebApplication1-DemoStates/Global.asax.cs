e8136e3a8229a9e2ffe91221d52f90a03f68bbbc
Samikshya <write2samikshyakar@gmail.com>
2021-10-28 22:09:11 +0530

    Add project files.

---
+++ WebApplication1-DemoStates/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication1_DemoStates
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+            Application["TotalApplications"] = 0;
+            Application["TotalUserSessions"] = 0;
+            Application["TotalApplications"] = (int)Application["TotalApplications"] + 1;
+        }
+
+        void Session_Start(Object sender, EventArgs e)
+
+        {
+
+            Application["TotalUserSessions"] = (int)Application["TotalUserSessions"] + 1;
+
+
+        }
+
+        void Session_End(Object sender, EventArgs e)
+        {
+
+            Application["TotalUserSessions"] = (int)Application["TotalUserSessions"] - 1;
+
+        }
+    }
+}
